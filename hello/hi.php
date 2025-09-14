78a05cc661f47df7a30f7c7d858a3197f1b1f41b
Matheus Luiz M. Silva <131410397+Kei2077@users.noreply.github.com>
2025-09-14 19:09:21 -0300

    Add CSS styling and PHP echo to hi.php
    Added inline CSS to style the h1 element and replaced the static h1 text with a new label. Introduced a PHP block to echo an h2 element, demonstrating PHP usage within the HTML.

--- hello/hi.php
+++ hello/hi.php
@@ -4,8 +4,17 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Exemplo php</title>
+    <style>
+        h1{
+            color: black;
+            text-shadow: 1px 1px 1px black;
+        }
+    </style>
 </head>
 <body>
-    <h1>Testando php</h1>
+    <h1>Testando:</h1>
+    <?php
+        echo "<h2>Testando php</h2>"; // pode usar tag 
+    ?>
 </body>
 </html>
